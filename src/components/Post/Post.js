24ce5e12c8e4ef1193feb0444098acfd9fc03804
Trizha Kate Longaza <trizha.longaza@boom.camp>
2019-06-19 17:46:41 +0800

    react-3: completed

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -56,6 +56,7 @@ export default class Post extends Component {
     // const editing = this.state.editing
     // const showMasterMenu = this.state.showMasterMenu
     const { editing, showMasterMenu } = this.state;
+    const { text, date } = this.props;
 
     return (
       // Main body of post
@@ -70,7 +71,7 @@ export default class Post extends Component {
             style={{ display: showMasterMenu ? 'flex' : 'none' }}
           >
             <span onClick={this.showEdit}>Edit</span>
-            <span>Delete</span>
+            <span onClick={() => this.props.deletePostFn(this.props.id)}>Delete</span>
           </div>
         </div>
 
@@ -83,7 +84,7 @@ export default class Post extends Component {
           <span className="Post__name">DevMountain</span>
           <span className="Post__handle">@DevMountain</span>
 
-          <span className="Post__date">- POST DATE GOES HERE</span>
+          <span className="Post__date">- {date}</span>
         </div>
 
         {/* This is where the text goes. Notice the turnary statement. The turnary statement decides to display either the text OR the editor view
@@ -95,11 +96,15 @@ export default class Post extends Component {
               }
         */}
         <div className="Post__content">
-          {// This has been pulled off of this.state via destructuring
-          editing ? (
-            <Edit text="" hideEdit={this.hideEdit} />
+          {editing ? (
+            <Edit
+              text={text}
+              id={this.props.id}
+              hideEdit={this.hideEdit}
+              updatePostFn={this.props.updatePostFn}
+            />
           ) : (
-            <span className="Post__text">POST TEXT GOES HERE</span>
+            <span className="Post__text">{text}</span>
           )}
         </div>
 
